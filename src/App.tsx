ae9f822ec1fffa70c20530cc846d144cd137120a
Qaisar Abbas <qaiser.abbas@aquila360.com>
2025-03-07 16:24:51 +0500

    create new book exchange project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
@@ -10,6 +11,12 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div className="d-flex gap-3">
+          <Button variant="primary" className="">
+            Primary
+          </Button>
+          <Button variant="secondary">Secondary</Button>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
